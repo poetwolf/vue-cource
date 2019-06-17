edfd116236648167eaca7d56d808961ccd311b56
marshalWalf <2532637185@qq.com>
2019-06-17 17:45:06 +0800

    Init Project

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+const resolve = dir => path.join(__dirname, dir)
+
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/iview-admin' : '/'
+
+module.exports = {
+  lintOnSave: false,
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  //打包时不生成.map文件
+  productionSourceMap: false
+}
